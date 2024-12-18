0020539736805ecf6840a7ab771298c30c543846
Debrah-exclusive <debrah.d747@gmail.com>
2024-12-18 12:00:45 +0000

    Ahhh

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -4,11 +4,32 @@
 package org.example;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        Library library = new Library();
+
+        // Add books to the library
+        library.addBook(new Book("A farmer's guide", "Chris Anorlds", 2000));
+        library.addBook(new Book("Alpha Male", "Andrew Tate", 2023));
+
+        // List of all books in the library
+        System.out.println("Books in the library:");
+        library.listBooks();
+
+        // Creating a patron
+        Patron patron = new Patron("Kwesi", 1);
+
+        // Borrowing a book
+        patron.borrowBook(new Book("A farmer's guide", "Chris Anorlds", 2000), library);
+
+        // List of borrowed books
+        System.out.println("\nBorrowed books:");
+        patron.listBorrowedBooks();
+
+        // Return the book
+        patron.returnBook(new Book("A farmer's guide", "Chris Anorlds", 2000), library);
+
+        // Final library state
+        System.out.println("\nBooks in the library after operations:");
+        library.listBooks();
     }
 }
